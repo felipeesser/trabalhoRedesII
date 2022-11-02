234d045176f96f58fdd92fd13b1dfc0d7c985358
marcValentino <mvalentino@id.uff.br>
2022-11-02 20:23:00 -0300

    added capability to communicate to other IPs

--- audioReceiver.py
+++ audioReceiver.py
@@ -3,7 +3,7 @@ from audioStream import *
 
 if __name__ == '__main__':
   inSocket = socket.socket(type=socket.SOCK_DGRAM) #socket udp
-  inSocket.bind(('127.0.0.1', 4000))
+  inSocket.bind(('0.0.0.0', 4000))
   outStream = StreamManager.startStream(False)
   while True:
     data = inSocket.recv(StreamManager.CHUNK * 4)
--- audioTransmitter.py
+++ audioTransmitter.py
@@ -4,7 +4,9 @@ from audioStream import *
 if __name__ == '__main__':
   inSocket = socket.socket(type=socket.SOCK_DGRAM)
   inStream = StreamManager.startStream(True)
+  # inSocket.bind(('0.0.0.0', 4000)) # descomentar se quiser transmitir para outro IP
+  ip = '127.0.0.1'
   while True:
     data = inStream.read(StreamManager.CHUNK)
     print('Buffer size: {}'.format(len(data)))
-    inSocket.sendto(data, ('127.0.0.1', 4000))
+    inSocket.sendto(data, (ip, 4000))
